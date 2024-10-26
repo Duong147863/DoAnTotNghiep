d482ae19c047b79014a5be84fcb71edee8534271
Nguyễn Bình Dương <126185898+Duong147863@users.noreply.github.com>
2024-10-27 00:06:27 +0700

    Update

--- lib/views/custom_widgets/custom_card.dart
+++ lib/views/custom_widgets/custom_card.dart
@@ -29,13 +29,13 @@ class ProjectCard extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 8.0),
-              Text(
-                'Thực hiện: ${project.departmentId ?? 'Chưa xác định'}',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.grey,
-                ),
-              ),
+              // Text(
+              //   'Thực hiện: ${project.departmentId ?? 'Chưa xác định'}',
+              //   style: TextStyle(
+              //     fontSize: 16.0,
+              //     color: Colors.grey,
+              //   ),
+              // ),
             ],
           ),
         ),
