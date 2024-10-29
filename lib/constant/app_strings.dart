3e1556ca24705e942662a91f06906c16a340f3d5
Nguyễn Bình Dương <126185898+Duong147863@users.noreply.github.com>
2024-10-29 15:58:32 +0700

    Update add_profile_screen

--- lib/constant/app_strings.dart
+++ lib/constant/app_strings.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   AppStrings._();
 
-  static const String baseUrlApi = "http://192.168.1.38:8000/api/v1/";
+  static const String baseUrlApi = "http://192.168.1.17:8000/api/v1/";
 
   static String notificationsKey = "notifications";
 
